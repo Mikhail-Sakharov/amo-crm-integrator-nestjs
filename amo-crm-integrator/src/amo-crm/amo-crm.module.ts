de169a20d28e4d9e7da90f1eb0bb7a59a3c3a851
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-11-21 18:54:01 +0300

    добавляет модуль, контроллер и сервис для работы с amoCRM API

---
+++ amo-crm-integrator/src/amo-crm/amo-crm.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { AmoCrmController } from './amo-crm.controller';
+import { AmoCrmService } from './amo-crm.service';
+
+@Module({
+  controllers: [AmoCrmController],
+  providers: [AmoCrmService],
+})
+export class AmoCrmModule {}
